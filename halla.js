b6b9be483d5465b076ed0aa3a798baafcb9e3887
stuartervine <stuart.ervine@shift-f6.com>
2017-06-08 22:45:08 +0100

    Removing spurious docker ps.

--- halla.js
+++ halla.js
@@ -108,7 +108,6 @@ module.exports = {
                     let targetSaved = data.toString().match(/(target saved)/gm);
                     if (targetSaved) {
                         console.log('Logging into Concourse: ' + concourseUrl);
-                        console.log(execSync('docker ps -a').toString());
                         resolve(remoteConcourse(target));
                     }
                 }
